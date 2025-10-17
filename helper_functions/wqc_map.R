5923a31dc86057cc5dab7da5402918ef71d0af9b
Regina Lionheart <38685718+R-Lionheart@users.noreply.github.com>
2025-10-16 20:51:59 -0700

    Tidy and comment wqc_map.R

--- helper_functions/wqc_map.R
+++ helper_functions/wqc_map.R
@@ -1,18 +1,17 @@
-#functions to compare to water quality criteria and map results
-#also a summary table
+## Functions to compare to water quality criteria and map results, and also a summary table
+# Requires: tidyverse
 
 wqc_comparison <- function(streams_wq_dat, streams_sites, input) {
   streams_wq_dat %>%
     filter(
       WaterYear == input$wqc_sum_year &
-        parameter %in%
-          c(
-            'Temperature, water',
-            'Dissolved Oxygen',
-            'pH',
-            'E. coli',
-            'Fecal Coliform'
-          )
+        parameter %in% c(
+          "Temperature, water",
+          "Dissolved Oxygen",
+          "pH",
+          "E. coli",
+          "Fecal Coliform"
+        )
     ) %>%
     left_join(streams_sites %>% select(SITE_CODE, AquaticLifeUse)) %>%
     group_by(SITE_CODE, AquaticLifeUse, parameter) %>%
@@ -35,22 +34,22 @@ wqc_comparison <- function(streams_wq_dat, streams_sites, input) {
     unnest(WQC_Output)
 }
 
-#wqc_comparison(streams_wq_dat,input=list(wqc_sum_year=2022))
+# wqc_comparison(streams_wq_dat, input = list(wqc_sum_year = 2022))
 
 wqc_map <- function(wqc_map_out, stream_sites, input) {
   pal_WQC <- colorFactor(
-    c('darkred', 'darkgreen', 'grey'),
-    levels = c('Violation(s)', "No Violation", 'Not Measured')
+    c("darkred", "darkgreen", "grey"),
+    levels = c("Violation(s)", "No Violation", "Not Measured")
   )
 
   wqc_parm <- input$wqc_sum_parm
-  if (input$wqc_sum_parm == 'All') {
+  if (input$wqc_sum_parm == "All") {
     wqc_parm <- c(
-      'Temperature, water',
-      'Dissolved Oxygen',
-      'pH',
-      'E. coli',
-      'Fecal Coliform'
+      "Temperature, water",
+      "Dissolved Oxygen",
+      "pH",
+      "E. coli",
+      "Fecal Coliform"
     )
   }
 
@@ -66,44 +65,34 @@ wqc_map <- function(wqc_map_out, stream_sites, input) {
       parameter = factor(
         parameter,
         levels = c(
-          'Temperature, water',
-          'Dissolved Oxygen',
-          'pH',
-          'E. coli',
-          'Fecal Coliform'
+          "Temperature, water",
+          "Dissolved Oxygen",
+          "pH",
+          "E. coli",
+          "Fecal Coliform"
         )
       )
     ) %>%
     pivot_wider(
       names_from = parameter,
       values_from = Notes,
-      names_expand = T,
-      values_fn = ~ ifelse(is.na(.x), '', .x),
-      values_fill = 'Not Measured'
+      names_expand = TRUE,
+      values_fn = ~ ifelse(is.na(.x), "", .x),
+      values_fill = "Not Measured"
     ) %>%
     summarise(
       Violation = ifelse(
         is.na(nViolation),
-        'Not Measured',
-        ifelse(nViolation > 0, 'Violation(s)', "No Violation")
+        "Not Measured",
+        ifelse(nViolation > 0, "Violation(s)", "No Violation")
       ),
       Text = paste0(
-        unique(AquaticLifeUse),
-        '<br>',
-        'Temperature: ',
-        `Temperature, water`,
-        '<br>',
-        'Dissolved Oxygen: ',
-        `Dissolved Oxygen`,
-        '<br>',
-        'pH: ',
-        `pH`,
-        '<br>',
-        'Fecal Coliform: ',
-        `Fecal Coliform`,
-        '<br>',
-        'E. coli: ',
-        `E. coli`
+        unique(AquaticLifeUse), "<br>",
+        "Temperature: ", `Temperature, water`, "<br>",
+        "Dissolved Oxygen: ", `Dissolved Oxygen`, "<br>",
+        "pH: ", `pH`, "<br>",
+        "Fecal Coliform: ", `Fecal Coliform`, "<br>",
+        "E. coli: ", `E. coli`
       )
     ) %>%
     left_join(streams_sites) %>%
@@ -112,91 +101,24 @@ wqc_map <- function(wqc_map_out, stream_sites, input) {
       fillColor = ~ pal_WQC(Violation),
       fillOpacity = 0.9,
       weight = 1,
-      color = 'black',
+      color = "black",
       popup = ~ paste0(
-        "<h6>",
-        "<b>",
-        SITE_NAME,
-        '<br>',
-        "</b>",
-        "</h6>",
-        '<br>',
-        SITE_CODE,
-        '<br>',
-        Text
+        "<h6><b>", SITE_NAME, "<br></b></h6>",
+        "<br>", SITE_CODE, "<br>", Text
       ),
       layerId = ~SITE_CODE,
       label = ~SITE_NAME
     ) %>%
-    addProviderTiles('Esri.NatGeoWorldMap') %>%
+    addProviderTiles("Esri.NatGeoWorldMap") %>%
     addLegend(
       pal = pal_WQC,
       values = factor(
-        c('Violation(s)', "No Violation", 'Not Measured'),
-        levels = c('Violation(s)', "No Violation", 'Not Measured')
+        c("Violation(s)", "No Violation", "Not Measured"),
+        levels = c("Violation(s)", "No Violation", "Not Measured")
       ),
-      title = 'Legend'
+      title = "Legend"
     )
 }
 
-# wqc_map(wqc_map_out = wqc_comparison(streams_wq_dat,stream_sites,input=list(wqc_sum_year=2022)),
-#         stream_sites)
-
-wqc_table <- function(wqc_map_out, input) {
-  if ("nViolation" %in% colnames(wqc_map_out)) {
-    wqc_map_out %>%
-      mutate(
-        parameter = factor(
-          parameter,
-          levels = c(
-            'Temperature, water',
-            'Dissolved Oxygen',
-            'pH',
-            'E. coli',
-            'Fecal Coliform'
-          )
-        )
-      ) %>%
-      group_by(parameter) %>%
-      summarise(
-        `Number of Sites with Violations` = length(which(nViolation > 0)),
-        `Number of Site Monitored` = length(nViolation)
-      ) %>%
-      complete(
-        parameter,
-        fill = list(
-          `Number of Sites with Violations` = NA,
-          `Number of Site Monitored` = 0
-        )
-      )
-  } else {
-    wqc_map_out %>%
-      mutate(nViolation = NA) %>%
-      mutate(
-        parameter = factor(
-          parameter,
-          levels = c(
-            'Temperature, water',
-            'Dissolved Oxygen',
-            'pH',
-            'E. coli',
-            'Fecal Coliform'
-          )
-        )
-      ) %>%
-      group_by(parameter) %>%
-      summarise(
-        `Number of Sites with Violations` = length(which(nViolation > 0)),
-        `Number of Site Monitored` = length(nViolation)
-      ) %>%
-      complete(
-        parameter,
-        fill = list(
-          `Number of Sites with Violations` = NA,
-          `Number of Site Monitored` = 0
-        )
-      )
-  }
-}
-
-#wqc_table(wqc_map_out = wqc_comparison(streams_wq_dat,input=list(wqc_sum_year=2022)))
+# wqc_map(
+#   wqc_map_out = wqc_comparison(streams_w
