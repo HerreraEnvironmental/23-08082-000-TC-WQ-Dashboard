e8f1a4acde99ca039afefd95f6fa11ac7cedae57
clarkbar88 <tclark@herrerainc.com>
2026-05-27 17:57:33 -0700

    data update for 2026-05-27

--- deploy_app.R
+++ deploy_app.R
@@ -3,28 +3,30 @@
 source("install_deps.R")
 source("sourcing_scripts/data_prep.R")
 
+#no longer needed to manually deploy for posit connect!
+
 ## Identify branch for merge
-branch <- system("git rev-parse --abbrev-ref HEAD", intern = TRUE)
+#branch <- system("git rev-parse --abbrev-ref HEAD", intern = TRUE)
 
 ## Only launch main branch
-if (branch == "main") {
-  rsconnect::deployApp(
-    appFiles = c(
-      "ui.R",
-      "server.R",
-      "disclaimer.txt",
-      "sourcing_scripts/",
-      "helper_functions/",
-      "outputs/",
-      #' wqi_function.R',
-      "www/"
-    ),
-    appId = 9060286,
-    account = "herrerainc",
-    server = "shinyapps.io",
-    appName = "ThurstonWQDashboard_Streams_Dev",
-    appTitle = "ThurstonWQDashboard_Streams_Dev"
-  )
-} else {
-  stop("Deployments are only allowed from main branch!")
-}
+# if (branch == "main") {
+#   rsconnect::deployApp(
+#     appFiles = c(
+#       "ui.R",
+#       "server.R",
+#       "disclaimer.txt",
+#       "sourcing_scripts/",
+#       "helper_functions/",
+#       "outputs/",
+#       #' wqi_function.R',
+#       "www/"
+#     ),
+#     appId = 9060286,
+#     account = "herrerainc",
+#     server = "shinyapps.io",
+#     appName = "ThurstonWQDashboard_Streams_Dev",
+#     appTitle = "ThurstonWQDashboard_Streams_Dev"
+#   )
+# } else {
+#   stop("Deployments are only allowed from main branch!")
+# }
